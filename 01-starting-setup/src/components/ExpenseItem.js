e6a01482bc84973821e88ebb4109e5767a164003
Taylor Casse <taylorcasse@gmail.com>
2023-05-11 15:14:13 +0200

    Working on basic expense tracker

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
-import './ExpenseItem.css'
-function ExpenseItem() {
-    return (
-        <div className="expense-item">
-            <div>March 28, 2023</div>
-            <div className="expense-item__description">
-                <h2>Car insurance</h2>
-                <div className="expense-item__price">R233</div>
-            </div>
+import "./ExpenseItem.css";
+import SquareDate from "./SquareDate";
 
-        </div>
-    );
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <SquareDate date={props.date}></SquareDate>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">R{props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
