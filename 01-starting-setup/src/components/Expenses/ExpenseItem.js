02c12476c7686a0fe2507f6f3aa4200f11cf760b
Taylor Casse <taylorcasse@gmail.com>
2023-05-12 12:29:57 +0200

    Made Card wrapper component and restructured the project

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,16 @@
 import "./ExpenseItem.css";
 import SquareDate from "./SquareDate";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <SquareDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">R{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
