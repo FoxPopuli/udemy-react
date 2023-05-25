600eb447ed8436fe8ffdbb0b65f9e56ee89b0dc9
Taylor Casse <taylorcasse@gmail.com>
2023-05-25 16:39:55 +0200

    Changed square date back to tutorial styling

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,7 +8,7 @@ const ExpenseItem = (props) => {
       <SquareDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">R{props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
   );
--- src/components/Expenses/SquareDate.js
+++ src/components/Expenses/SquareDate.js
@@ -6,10 +6,10 @@ const SquareDate = (props) => {
   const year = props.date.getFullYear();
 
   return (
-    <div className="main">
-      <div className="day">{day}</div>
-      <div className="month">{month}</div>
-      <div className="year">{year}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__year">{year}</div>
     </div>
   );
 };
